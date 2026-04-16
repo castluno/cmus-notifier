9a126b9752b5cb7046fa66f74476a902e44976f8
castluno <castluno@xyecoc.com>
2026-04-17 00:17:30 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,25 +20,13 @@ git clone https://github.com/castluno/cmus-notifier
 mkdir -p "$HOME/.cn-dont-delete"
 
 # cloning files from the repository
-cp "cmus-notifier/controller.sh" "$HOME/.cn-dont-delete"
+chmod +x "cmus-notifier/controller.sh"
+chmod +x "cmus-notifier/notifier.sh"
+cp "cmus-notifier/controller.sh" "$PREFIX/bin/cn"
 cp "cmus-notifier/notifier.sh" "$HOME/.cn-dont-delete"
 cp "cmus-notifier/readme_indir.txt" "$HOME/.cn-dont-delete/readme.txt"
 
 rm -rf "cmus-notifier"
-
-#writing a startup command in .bashrc and .zshrc
-touch "$HOME/.bashrc" "$HOME/.zshrc"
-ALIAS_LINE='alias cn="bash ~/.cn-dont-delete/controller.sh"'
-grep -qxF "$ALIAS_LINE" "$HOME/.bashrc" || echo "$ALIAS_LINE" >> "$HOME/.bashrc"
-grep -qxF "$ALIAS_LINE" "$HOME/.zshrc" || echo "$ALIAS_LINE" >> "$HOME/.zshrc"
-
-if [[ "$SHELL" == *"zsh"* ]]; then
-  echo 'restart your terminal or type "source ~/.zshrc"'
-  echo
-else
-  echo 'restart your terminal or type "source ~/.bashrc"'
-  echo
-fi
 
 echo
 echo 'cmus-notifier is installed. do not delete ~/.cn-dont-delete - the program files are located here.'
